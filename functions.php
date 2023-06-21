c61658a5b029c6f65059b708ad52ee57300edb90
GianbaBotassi <gianba89@gmail.com>
2023-06-21 16:17:51 +0200

    Print pwd into html

--- functions.php
+++ functions.php
@@ -12,4 +12,5 @@ function getRandPwd($pwdLen){
   return $randStr;
 }
 
+
 ?>
--- index.php
+++ index.php
@@ -6,17 +6,21 @@
   <title>PHP Strong Password Generator</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 
-  <?php
-
-  include "functions.php";
-
+  <?php include "functions.php"; 
   ?>
+
 </head>
 <body>
   <div class="container">
     <h1 class="text-center">Strong Password Generator</h1>
     <h2 class="text-center">Genera una password sicura</h2>
-    <div class="text-center my-4">Output</div>
+    <h3 class="text-center my-4">
+    <?php
+      $pwdLen = $_GET['pwd-len'];
+      $ranPwd = (getRandPwd($pwdLen));
+      echo $ranPwd;
+      ?>
+    </h3>
     <form>
       <div class="row">
         <div  class="col-6">Lunghezza password</div>
@@ -35,26 +39,6 @@
     </div>
     </form>
 
-  <?php
 
-  $pwdLen = $_GET['pwd-len'];
-
-  echo "<br />";
-
-  
-    echo "<br />";
-
-    var_dump(getRandPwd($pwdLen));
-
-
-
-  
-
-  // echo "<pre>";
-  // var_dump($pwd);
-  // echo "</pre>";
-  // $pwd = ranPwd();
-  // var_dump($pwd);
-  ?>
 </body>
 </html>
