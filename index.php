c35649dda6a91ca46e7cb200eb62aad05a753bdd
GianbaBotassi <gianba89@gmail.com>
2023-06-21 16:01:01 +0200

    Print pass with 8 characters/numbers

--- index.php
+++ index.php
@@ -11,19 +11,53 @@
     <h1 class="text-center">Strong Password Generator</h1>
     <h2 class="text-center">Genera una password sicura</h2>
     <div class="text-center my-4">Output</div>
-    <div class="row">
-      <div  class="col-6">Lunghezza password</div>
-      <input class="offset-2 col-4" type="text">
-    </div>
-    <!-- <div class="row my-3">
-      <div class="col-6">Consenti ripetizioni di uno o più caratteri</div>
-      <div class="offset-2 col-4">
-        <input type="radio" name="more-character" id="more-character-yes">
-        <label for="more-character-yes">Si</label>
-        <input type="radio" name="more-character" id="more-character-no">
-        <label for="more-character-no">No</label> -->
+    <form>
+      <div class="row">
+        <div  class="col-6">Lunghezza password</div>
+        <input class="offset-2 col-4" type="text" name="pwd-len">
       </div>
+      <!-- <div class="row my-3">
+        <div class="col-6">Consenti ripetizioni di uno o più caratteri</div>
+        <div class="offset-2 col-4">
+          <input type="radio" name="more-character" id="more-character-yes">
+          <label for="more-character-yes">Si</label>
+          <input type="radio" name="more-character" id="more-character-no">
+          <label for="more-character-no">No</label>
+        </div>
+      </div> -->
+      <input type="submit" value="Send">
     </div>
-  </div>
+    </form>
+
+  <?php
+
+  $pwd = [];
+
+  echo "<br />";
+
+
+    $numWords = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    $strLength = strlen($numWords);
+    // var_dump($strLength);
+    $randStr = "";
+
+    $x = 0;
+    while (++$x <= 8) {
+      $randStr = $randStr . $numWords[rand(0,$strLength - 1)];
+    };
+    echo "<br />";
+
+    var_dump($randStr);
+
+
+
+  
+
+  // echo "<pre>";
+  // var_dump($pwd);
+  // echo "</pre>";
+  // $pwd = ranPwd();
+  // var_dump($pwd);
+  ?>
 </body>
 </html>
